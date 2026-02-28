aa562de6b47f9601182080ba0adcc88282de70cf
Vir Codificat <vircodificat@gmail.com>
2026-02-27 19:37:55 -0800

    Set memory from testbench.

--- ext/Ram16k.v
+++ ext/Ram16k.v
@@ -1,11 +1,17 @@
-module Rom16k(
+module Ram16k(
     input  wire        clock,
+    input  wire [15:0] in,
+    input  wire        load,
     input  wire [13:0] address,
     output reg  [15:0] out
 );
     reg [15:0] memory [0:16383];
 
     always @(posedge clock) begin
+        if (load) begin
+            memory[address] <= in;
+        end
+
         out <= memory[address];
     end
 endmodule
